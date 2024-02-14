49e43b5ff0f29598cec51859446a6a773358d772
1AndOne1 <112853915+1AndOne1@users.noreply.github.com>
2024-02-14 05:27:18 +0500

    Add files via upload

---
+++ chat/host/Host1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace host
+{
+    internal class Host1
+    {
+        static async void Main(string[] args)
+        {
+                TcpListener server = new TcpListener(IPAddress.Any, 3000);
+                server.Start();
+                while (true)
+                {
+                    try
+                    {
+                        TcpClient client = await server.AcceptTcpClientAsync();
+                        NetworkStream stream = client.GetStream();
+                    }
+                    catch
+                    {
+                        server.Stop();
+                        break;
+                    }
+                }
+                if (!true)
+                {
+                    server.Stop();
+                }
+        }
+    }
+}
